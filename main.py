ba495a865af4e56c9d37007594016edcad9e86d8
CalmlyCode <109759851+CalmlyCode@users.noreply.github.com>
2023-05-29 22:17:13 +0000

    Completed Day 1 task.

---
+++ main.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+print('Welcome to the Band Name Generator! \n\nThis is the day 1 task for the App Brewery Python Bootcamp. This program will ask two questions and then combine to form a Band name. \n')
+
+var_a = input('Name the first word that comes to mind: \n')
+var_b = input('What is the last thing you ate? \n')
+
+print('Your cool band name is ' + var_a + var_b)
